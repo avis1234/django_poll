f936d27a2dfd14637437a73c480042e0d614c292
Avi Segal <avisegal@gmail.com>
2012-08-22 18:54:54 +0300

    finished tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,13 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
-from django.http import HttpResponse
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+    
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
